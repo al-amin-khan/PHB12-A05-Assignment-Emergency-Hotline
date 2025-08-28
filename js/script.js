7fd11e3fb000c29f6194fcf6af1d2ca08b03315b
Al Amin Khan <alaminkhan2010@gmail.com>
2025-08-29 01:24:10 +0600

    add functional logics to copy, call buttons

--- js/script.js
+++ js/script.js
@@ -1,49 +1,69 @@
 let histories = [];
 
-const cards = document.getElementsByClassName('call');
-const historyContainer = document.getElementById('history');
-const coinElement = document.getElementById('available-coin');
-
+const cards = document.getElementsByClassName("call");
+const copyButtons = document.getElementsByClassName("btn-copy");
+const historyContainer = document.getElementById("history");
+const coinElement = document.getElementById("available-coin");
 
 for (let index = 0; index < cards.length; index++) {
     const card = cards[index];
-    card.addEventListener('click', function () {
-        const cardContent = card.parentElement.parentElement.parentElement.children;
+    card.addEventListener("click", function () {
+        const cardContent =
+            card.parentElement.parentElement.parentElement.children;
 
-        const ServiceTitle = cardContent[index].getElementsByTagName('p')[0].innerText;
-        const ServiceCallNumber = cardContent[index].getElementsByTagName('p')[2].innerText;
+        const ServiceTitle =
+            cardContent[index].getElementsByTagName("p")[0].innerText;
+        const ServiceCallNumber =
+            cardContent[index].getElementsByTagName("p")[2].innerText;
 
         const createdAt = new Date().toLocaleTimeString();
 
         const coin = coinElement.innerText;
         if (coin >= 0 && coin < 20) {
-            alert('Insufficient coins to make a call.');
+            alert("Insufficient coins to make a call.");
             return;
-        }
-        else {
+        } else {
             coinElement.innerText = parseInt(coin) - 20;
         }
 
-        histories.push({ServiceTitle, ServiceCallNumber, createdAt});
+        histories.push({ ServiceTitle, ServiceCallNumber, createdAt });
 
         const lastPushed = histories[histories.length - 1];
-        
 
         historyContainer.innerHTML += `
             <div class="flex justify-between items-center bg-[#fafafa] shadow-sm rounded-lg p-2 mt-3">
                 <div class="text-sm">
-                    <p>${lastPushed.ServiceTitle}</p>
-                    <p>${lastPushed.ServiceCallNumber}</p>
+                    <p class="font-semibold">${lastPushed.ServiceTitle}</p>
+                    <p class="font-semibold">${lastPushed.ServiceCallNumber}</p>
                 </div>
                 <div class="text-sm text-gray-500">
                     <p>${lastPushed.createdAt}</p>
                 </div>
             </div>
-        `
+        `;
 
-        document.getElementById('btn-clear').addEventListener('click', function (){
-            historyContainer.innerHTML = '';
-        })
-    })
+        document
+            .getElementById("btn-clear")
+            .addEventListener("click", function () {
+                historyContainer.innerHTML = "";
+            });
+    });
 }
 
+for (let index = 0; index < copyButtons.length; index++) {
+    const copyButton = copyButtons[index];
+    copyButton.addEventListener("click", async function () {
+        const cardContent =
+            copyButton.parentElement.parentElement.parentElement.children;
+        const ServiceCallNumber =
+            cardContent[index].getElementsByTagName("p")[2].innerText;
+        await navigator.clipboard.writeText(ServiceCallNumber)
+        .then( res => {
+            alert("Phone number copied to clipboard: " + ServiceCallNumber);
+        });
+
+        let totalCopy = document.getElementById("total-copy").innerText;
+        totalCopy++;
+        document.getElementById("total-copy").innerText = totalCopy;
+    });
+}
